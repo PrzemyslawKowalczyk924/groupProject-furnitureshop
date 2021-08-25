5dcca1da8455b73bdebeba2dbae4d58236413aeb
unknown <kowalczyk.przemek@wp.pl>
2021-08-25 21:54:59 +0200

    Initial commit

---
+++ precache-manifest.1358a528cdf2910e3ff731ef1f5b19ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec538dfabeec9e2510b857ecb47689b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e0aabb496c78df1fafe",
+    "url": "/static/css/2.12c7031d.chunk.css"
+  },
+  {
+    "revision": "1de01b6848ed5e23679e",
+    "url": "/static/css/main.d70b7342.chunk.css"
+  },
+  {
+    "revision": "4e0aabb496c78df1fafe",
+    "url": "/static/js/2.a42a4a0c.chunk.js"
+  },
+  {
+    "revision": "1de01b6848ed5e23679e",
+    "url": "/static/js/main.8eddfe62.chunk.js"
+  },
+  {
+    "revision": "443ca399ce3454ce1325",
+    "url": "/static/js/runtime-main.965ca4a5.js"
+  }
+]);
