51c1a879e4a2a9fa3eb81edd35a8ea4df0ff4af5
unknown <kowalczyk.przemek@wp.pl>
2021-10-06 21:13:33 +0200

    Add props

--- src/components/features/SearchInput/SearchInput.js
+++ src/components/features/SearchInput/SearchInput.js
@@ -4,14 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import styles from './SearchInput.module.scss';
 
-const SearchInput = () => (
-  <div className={styles.searchField}>
-    <input placeholder='Search products...' type='text' />
-    <a href='/search'>
-      <FontAwesomeIcon /* className={styles.icon} */ icon={faSearch} />
-    </a>
-  </div>
-);
+const SearchInput = props => {
+  const { research } = props;
+
+  return (
+    <div className={styles.searchField}>
+      <input placeholder='Search products...' type='text' />
+      <a href='/search'>
+        <FontAwesomeIcon /* className={styles.icon} */ icon={faSearch} />
+      </a>
+    </div>
+  );
+};
 
 SearchInput.propTypes = {
   children: PropTypes.node,
